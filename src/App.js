70a4aa71e30c4391f98af57ed001dc9b2e66c932
BruceHill <brucehillwalley@gmail.com>
2023-12-13 02:39:14 +0300

    develops

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import "./App.css"
+import "./App.css";
+import debounce from "lodash/debounce";
 
 function App() {
   const [cards, setCards] = useState([]);
@@ -12,13 +13,22 @@ function App() {
   }, []);
 
   const shuffleCards = () => {
-    const animals = ['cat', 'dog', 'lion', 'tiger', 'elephant', 'giraffe', 'panda', 'zebra'];
+    const animals = [
+      "cat",
+      "dog",
+      "lion",
+      "tiger",
+      "elephant",
+      "giraffe",
+      "panda",
+      "zebra",
+    ];
     const doubleAnimals = animals.concat(animals);
     const shuffled = doubleAnimals.sort(() => Math.random() - 0.5);
     return shuffled.map((animal) => ({ id: Math.random(), animal }));
   };
 
-  const handleClick = (card) => {
+  const handleClick = debounce((card) => {
     if (!matchedCards.includes(card) && flippedCards.length < 2) {
       setFlippedCards([...flippedCards, card]);
     }
@@ -31,21 +41,30 @@ function App() {
         setTimeout(() => setFlippedCards([]), 1000);
       }
     }
-  };
+  }, 500);
 
-  const isCardFlipped = (card) => flippedCards.includes(card) || matchedCards.includes(card);
+  const isCardFlipped = (card) =>
+    flippedCards.includes(card) || matchedCards.includes(card);
 
   return (
     <div className="App">
-      <h1>Memory Game</h1>
+      <h1 className="text-center display-4 mb-5 mt-5 text-success">
+        Memory Game
+      </h1>
       <div className="game-board">
         {cards.map((card) => (
           <div
             key={card.id}
-            className={`card ${isCardFlipped(card) ? 'flipped' : ''}`}
+            className={`card ${isCardFlipped(card) ? "flipped" : ""}`}
             onClick={() => handleClick(card)}
           >
-            {isCardFlipped(card) && <img src={`images/${card.animal}.jpeg`} alt={card.animal} />}
+            {isCardFlipped(card) && (
+              <img
+                src={`images/${card.animal}.jpeg`}
+                alt={card.animal}
+                className="img-thumbnail"
+              />
+            )}
           </div>
         ))}
       </div>
